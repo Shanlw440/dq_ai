ed1e7c836887e0cb93319b30714688f2c812d1ad
Shannon Wise <s.wise11@hotmail.co.uk>
2025-08-16 15:35:41 +0100

    Expose server for Gunicorn

--- app.py
+++ app.py
@@ -420,3 +420,7 @@ def render_filtered(sev_filter, store):
 # -----------------------
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=8050)
+server = app.server
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=8050)
